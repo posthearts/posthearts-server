eea196ca1c131dbbee4633af031ee20f70ba22db
AyomiCoder <alukostephen01@gmail.com>
2025-02-01 10:39:00 +0100

    Configure CORS to allow requests from localhost:5173

--- app.ts
+++ app.ts
@@ -30,6 +30,15 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api', authRoutes);
 app.use('/api', letterRoutes);
 
+// Set headers manually for CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
