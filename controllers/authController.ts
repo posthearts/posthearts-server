b1beab9ccd7919ed2118cc46ef54982a81aba5c4
AyomiCoder <alukostephen01@gmail.com>
2025-02-01 10:25:54 +0100

    Refactor Google authentication response to return user info and token as JSON

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import User from '../models/User'; // Adjust the import based on your project structure
+import User from '../models/User';
 
 interface User {
   name: string;
@@ -15,15 +15,14 @@ const generateJWT = (user: any): string => {
 export const googleAuth = (req: Request, res: Response, next: NextFunction): void => {
   try {
     const user = req.user as User;
-    if (!user) {
-      throw new Error('No user provided');
-    }
-    console.log('User:', user); // Log the user object
     const token = generateJWT(user);
-    console.log('Generated Token:', token); // Log the generated token
-    const redirectUrl = `https://posthearts.vercel.app/api/auth?token=${token}&name=${encodeURIComponent(user.name)}&profile_picture=${encodeURIComponent(user.profile_picture)}`;
-    console.log('Redirect URL:', redirectUrl); // Log the redirect URL
-    res.redirect(redirectUrl);
+    res.json({
+      token,
+      user: {
+        name: user.name,
+        profile_picture: user.profile_picture
+      }
+    });
   } catch (err) {
     next(err);
   }
